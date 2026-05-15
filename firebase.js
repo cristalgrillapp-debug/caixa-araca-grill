365cfec1f8e49f64ebfabc295c883680009c590f
cristalgrillapp-debug <cristalgrillapp@gmail.com>
2026-05-15 06:47:31 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAx9W-A2l2qqHZthQxG48yuhqFBwLqAo5U",
+  authDomain: "caixagrill-7f947.firebaseapp.com",
+  projectId: "caixagrill-7f947",
+  storageBucket: "caixagrill-7f947.firebasestorage.app",
+  messagingSenderId: "601068891689",
+  appId: "1:601068891689:web:2e9b038655c588caacb638"
 }
 
 const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
-export default app
+
+export const db      = getFirestore(app)
+export const storage = getStorage(app)
